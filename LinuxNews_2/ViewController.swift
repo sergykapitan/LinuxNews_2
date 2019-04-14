54c619da688cab28854cc3deccc1bbec3e6174f9
sergykapitan <12kaptan12@gmail.com>
2019-04-14 18:00:12 +0300

    Initial Commit

---
+++ LinuxNews_2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LinuxNews_2
+//
+//  Created by Sergey Koriukin on 14.04.2019.
+//  Copyright © 2019 Sergey Koriukin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
